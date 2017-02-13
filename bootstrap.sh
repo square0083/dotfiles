dd7e81e361e8a48e804f5b2a3d44196c8a4d5553
Liang Lei <square0083@gmail.com>
2017-02-13 12:45:17 +0800

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,3 +12,15 @@ function doIt() {
 				-avh --no-perms . ~;
 	source ~/.zshrc
 }
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+
+unset doIt;
