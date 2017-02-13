c3655d84d5e686c6db96a149a7bdbc376d2fc6e4
Liang Lei <square0083@gmail.com>
2017-02-13 12:35:16 +0800

    init

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master
+
+function doIt() {
+	rsync --exclude ".git/" \
+				--exclude ".DS_Store" \
+				--exclude ".osx" \
+				--exclude "bootstrap.sh" \
+				--exclude "README.md" \
+				--exclude "LICENSE-MIT.txt" \
+				-avh --no-perms . ~;
+	source ~/.zshrc
+}
---
+++ my_configs.sh
@@ -0,0 +1,9 @@
+alias pb="pythonbrew"
+alias dc="docker-compose"
+
+
+alias lg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glg="git log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
+
+export NVM_DIR="~/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
